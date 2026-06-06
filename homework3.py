45eec54945eebf394aa52f7965d9bcfc46da536e
sabaoqropiridze2003-create <sabaoqropiridze2003@gmail.com>
2026-06-07 00:57:07 +0400

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,20 @@
+# პირველი ამოცანა
+# sentence = input("enter a sentence: ")
+# first_word = sentence.split()[0]
+# second_word = sentence.split()[1]
+# new_sentence = sentence.replace(first_word, second_word)
+# print(new_sentence)
+
+# მეორე ამოცანა
+
+# sentence = input("enter a sentence: ")
+# words = sentence.split()
+# longest_word = max(words, key=len)
+# print(longest_word)
+
+# მესამე ამოცანა
+
+word_one = input("enter the first word: ")
+word_two = input("enter the second word: ")
+
+print(word_one[::-1] == word_two)
