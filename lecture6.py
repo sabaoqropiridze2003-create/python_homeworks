0e7e5f68fb3245e1063e2b8de204c16bd723c242
sabaoqropiridze2003-create <sabaoqropiridze2003@gmail.com>
2026-06-15 03:54:34 +0400

    Add files via upload

---
+++ lecture6.py
@@ -0,0 +1,146 @@
+# print("Start of program")
+
+# while 7 > 4:
+
+#     print("Hello world")
+
+# print("End of program")
+
+# a = 1
+
+# b = 5
+
+# while a <= b:
+#     print(a)
+#     a += 1
+# else:
+#     print("a is not less than b")
+
+# print("End of program")
+
+
+# a = 1
+
+# b = 5
+
+# while a <= b:
+#     print(a)
+# a += 1
+
+
+# print("End of program")
+
+# counter = 0
+
+# while counter < 7:
+#     print("Hello world!")
+#     counter += 1
+
+# password = "admin123"
+
+# while True:
+#     user_password = input("Please enter your password: ")
+
+#     if user_password == password:
+#         print("Welcome to the admin panel!")
+#         break
+#     else:
+#         print("Invalid password")
+# else:
+#     print("invalid pasword")
+
+# attempts = 3
+# password = "admin123"
+
+# while attempts > 0:
+#     user_password = input("enter your passwprd: ")
+#     if user_password == password:
+#         print("welcome to the admin pannel")
+#         break
+#     else:
+#         attempts -= 1
+#         print(f"Invalid password. You have {attempts} attempts left!")
+# else:
+#     print("Accsess denied. Too many attempts.")
+
+
+# attempts = 3
+# password = "admin123"
+
+# while attempts > 0:
+#     user_password = input("enter your passwprd: ")
+#     if user_password == password:
+#         print("welcome to the admin pannel")
+#         break
+#     else:
+#         if user_password == "":
+#             print("blank password is not allowed")
+#             print(f"you have {attempts} attempts left!")
+#             continue
+#         attempts -= 1
+#         print(f"Invalid password. You have {attempts} attempts left!")
+# else:
+#     print("Accsess denied. Too many attempts.")
+
+# text = "Hello world!"
+
+# for char in text:
+#     print(char)
+
+# for num in 598:
+#     print(num)
+
+
+# text = "Hello world!"
+
+# for char in text:
+#     print(char)
+
+# iterator = iter(text)
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+
+
+# for char in "python":
+#     print("Hellow world")
+
+
+# 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+# for i in ra
+
+# for _ in range(5):
+#     print("Hello world!")
+
+
+# for _ in range(5):
+#     number = int(input("enter the number: "))
+
+# for i in range(1, 10):
+#     print(i)
+
+# for i in range(1, 11, 2):
+#     print(i)
+
+# for i in range(1, 6):
+#     for j in range(1, 6):
+#         print(f"i = {i}, j = {j}")
+
+# from random import randint
+# import random
+
+# print(random.randint(1, 100))
+# print(random.random())
+# print(random.randrange(1, 10))
+
+
+# print(randint(1, 10))
